483a449843bebc6a5ff10e7a5c58cfddde03efa8
gym1702 <89650800+gym1702@users.noreply.github.com>
2023-02-16 16:03:53 -0600

    cambios

--- proyecto/settings/base.py
+++ proyecto/settings/base.py
@@ -69,7 +69,8 @@ INSTALLED_APPS = DJANGO_APPS + LOCAL_APPS + THIRD_PARTY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware', #para despliegue en render
+    #para despliegue en render comentar para trabajar en local
+    'whitenoise.middleware.WhiteNoiseMiddleware', 
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
